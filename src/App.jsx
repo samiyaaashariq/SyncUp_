1404452887414af2e84104f06ab6ca799869df74
Samiya Shariq <samiya.shariq31@gmail.com>
2026-06-17 17:26:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,5 @@ import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 
 export default function App() {
-  const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
-      setUser(currentUser);
-      setLoading(false);
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  if (loading) {
-    return (
-      <div style={{ padding: "20px" }}>
-        <h2>Loading SyncUp...</h2>
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      {!user ? <Auth /> : <Dashboard />}
-    </div>
-  );
+  return <h1>SyncUP is working</h1>
 }
