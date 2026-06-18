33a6554544ef18aec25c71e1bd34049b08fd565f
Samiya Shariq <samiya.shariq31@gmail.com>
2026-06-18 15:06:31 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCbmFYm-SJuqDsvTm2lp4mvtgCMYdKPuZs",
-  authDomain: "syncup-a6327.firebaseapp.com",
-  projectId: "syncup-a6327",
-  storageBucket: "syncup-a6327.firebasestorage.app",
-  messagingSenderId: "879637125727",
-  appId: "1:879637125727:web:560073b6c7c2ba6d1bd9ea"
+  apiKey: "AIzaSyCEm_M5YDGXJATaPmUS6PEfsj9EqACTmkg",
+  authDomain: "syncup-e31b6.firebaseapp.com",
+  projectId: "syncup-e31b6",
+  storageBucket: "syncup-e31b6.firebasestorage.app",
+  messagingSenderId: "382502575211",
+  appId: "1:382502575211:web:e6f207c74162259c3e252e"
 };
 
 const app = initializeApp(firebaseConfig);
