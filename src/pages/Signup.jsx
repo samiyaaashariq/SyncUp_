96dff24ee9935292074c8669323e296be7e8c29a
Samiya Shariq <samiya.shariq31@gmail.com>
2026-06-18 17:47:48 +0530

    Update Signup.jsx

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,19 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = async () => {
+    try {
+      const userCredential =
+        await createUserWithEmailAndPassword(
+          auth,
+          email,
+          password
+        );
+
+      alert("Account Created Successfully 🚀");
+      console.log(userCredential.user);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
     <div>
       <h2>Create Account</h2>
-      <input type="text" placeholder="Full Name" />
+
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
       <br /><br />
 
-      <input type="email" placeholder="Email" />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
       <br /><br />
 
-      <input type="password" placeholder="Password" />
-      <br /><br />
-
-      <button>Create Account</button>
+      <button onClick={handleSignup}>
+        Create Account
+      </button>
     </div>
   );
 }
