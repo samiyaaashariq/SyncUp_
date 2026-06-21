34ca6fc45eb6b483f839b5e45fec48eb5ff142f5
Samiya Shariq <samiya.shariq31@gmail.com>
2026-06-21 12:56:53 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
-import React from “react”;
-import { auth } from “../firebase”;
-import { useNavigate } from “react-router-dom”;
+import React from "react";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
 const nav = useNavigate();
