b828490fe56b86c4372ce675a7d9cddd9190bf32
Samiya Shariq <samiya.shariq31@gmail.com>
2026-06-23 23:57:02 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth, db } from "../firebase";
-import { sendNotification } from "../notifications";
+import { sendNotification } from "../Notifications";
 import {
   collection,
   addDoc,
