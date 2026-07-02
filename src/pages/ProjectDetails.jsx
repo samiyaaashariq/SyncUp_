c8cbfd0df63b4d34869318bad28ca04af69e31d5
Samiya Shariq <samiya.shariq31@gmail.com>
2026-07-02 13:58:34 +0530

    Update ProjectDetails.jsx

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -228,8 +228,7 @@ export default function ProjectDetails() {
       <style>{cssBlock}</style>
 
       <div className="pdContainer">
-        <button className="pdBackBtn" onClick={() => navigate(-1)}>← Back</button>
-
+        <<button className="pdBackBtn" onClick={() => navigate("/dashboard")}>← Back</button>
         <div className="pdHero">
           <div className="heroBlob heroBlobA" />
           <div className="heroBlob heroBlobB" />
